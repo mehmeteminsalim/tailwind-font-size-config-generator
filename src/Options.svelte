b4c646f62f32b5e37ce9f1a98a48fda47d70acee
Mehmet Emin Salim <mehmeteminsalim@gmail.com>
2022-02-03 02:13:41 +0300

    some-conf-added

--- src/Options.svelte
+++ src/Options.svelte
@@ -22,11 +22,15 @@
 </div>
 <div class="input-area-margin">
   <Select labelText="Scale" selected="g10">
-    <SelectItem value="white" text="White" />
-    <SelectItem value="g10" text="Gray 10" />
-    <SelectItem value="g80" text="Gray 80" />
-    <SelectItem value="g90" text="Gray 90" />
-    <SelectItem value="g100" text="Gray 100" />
+    <SelectItem value="minor-second" text="1.067 Minor Second" />
+    <SelectItem value="major-second" text="1.125 Major Second" />
+    <SelectItem value="minor-third" text="1.200 Minor Third" />
+    <SelectItem value="major-third" text="1.250 Major Third" />
+    <SelectItem value="perfect-fourth" text="1.333 Perfect Fourth" />
+    <SelectItem value="augmented-fourth" text="1.414 Augmented Fourth" />
+    <SelectItem value="perfect-fifth" text="1.500 Perfect Fifth" />
+    <SelectItem value="golden-ratio" text="1.618 Golden Ratio" />
+    <SelectItem value="custom" text="Custom..." />
   </Select>
 </div>
 <div class="input-area-margin">
--- src/Preview.svelte
+++ src/Preview.svelte
@@ -32,43 +32,43 @@
   </div>
   <div class="preview-item-container">
     <label for="h2">H2</label>
-    <h2 id="h2" class="h1-preview">{previewText}</h2>
+    <h2 id="h2" class="h2-preview">{previewText}</h2>
   </div>
   <div class="preview-item-container">
     <label for="h3">H3</label>
-    <h3 id="h3" class="h1-preview">{previewText}</h3>
+    <h3 id="h3" class="h3-preview">{previewText}</h3>
   </div>
   <div class="preview-item-container">
     <label for="h4">H4</label>
-    <h4 id="h4" class="h1-preview">{previewText}</h4>
+    <h4 id="h4" class="h4-preview">{previewText}</h4>
   </div>
   <div class="preview-item-container">
     <label for="h5">H5</label>
-    <h5 id="h5" class="h1-preview">{previewText}</h5>
+    <h5 id="h5" class="h5-preview">{previewText}</h5>
   </div>
   <div class="preview-item-container">
     <label for="h6">H6</label>
-    <h6 id="h6" class="h1-preview">{previewText}</h6>
+    <h6 id="h6" class="h6-preview">{previewText}</h6>
   </div>
   <div class="preview-item-container">
     <label for="blockquote">Blockquote</label>
-    <p id="blockquote" class="h1-preview">{previewText}</p>
+    <p id="blockquote" class="blockquote-preview">{previewText}</p>
   </div>
   <div class="preview-item-container">
     <label for="body">Body</label>
-    <p id="body" class="h1-preview">{previewText}</p>
+    <p id="body" class="body-preview">{previewText}</p>
   </div>
   <div class="preview-item-container">
     <label for="caption">Caption</label>
-    <p id="caption" class="h1-preview">{previewText}</p>
+    <p id="caption" class="caption-preview">{previewText}</p>
   </div>
   <div class="preview-item-container">
     <label for="endnote">Endnote</label>
-    <p id="endnote" class="h1-preview">{previewText}</p>
+    <p id="endnote" class="endnote-preview">{previewText}</p>
   </div>
   <div class="preview-item-container">
     <label for="footnote">Footnote</label>
-    <p id="footnote" class="h1-preview">{previewText}</p>
+    <p id="footnote" class="footnote-preview">{previewText}</p>
   </div>
 </div>
 
@@ -80,5 +80,28 @@
     margin-bottom: 20px;
     padding-bottom: 10px;
     border-bottom: 1px solid white;
+  }
+
+  .h1-preview {
+  }
+  .h2-preview {
+  }
+  .h3-preview {
+  }
+  .h4-preview {
+  }
+  .h5-preview {
+  }
+  .h6-preview {
+  }
+  .blockquote-preview {
+  }
+  .body-preview {
+  }
+  .caption-preview {
+  }
+  .endnote-preview {
+  }
+  .footnote-preview {
   }
 </style>
